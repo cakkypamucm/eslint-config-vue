1e7bd06fcfaf240f088c884bc559625cd75eef19
cakkypamucm <119052172+cakkypamucm@users.noreply.github.com>
2023-06-14 21:23:53 +0300

    git init

---
+++ README.md
@@ -0,0 +1,21 @@
+This is `vue2+` `eslint` config
+
+### Install
+
+```shell
+npm i --save-dev github:cakkypamucm/eslint-config-vue
+```
+
+### Use
+
+Add `eslint-config-vue-cakkypamucm` to the "extends" array in your `eslint` config file
+
+<pre>
+{
+    "extends": ["some-other-config-you-use", <b>"eslint-config-vue-cakkypamucm"</b>]
+}
+</pre>
+
+### For synergy
+
+Add [stylelint-config-vue-cakkypamucm](https://github.com/cakkypamucm/stylelint-config-vue) to your project
---
+++ index.js
@@ -0,0 +1,43 @@
+/* eslint sort-keys/sort-keys-fix: "error" */
+
+const { isVue3 } = require("vue-demi");
+
+module.exports = {
+    extends: [
+        `plugin:vue-scoped-css/${isVue3 ? "vue3-" : ""}recommended`,
+        `plugin:vue/${isVue3 ? "vue3-" : ""}recommended`
+    ],
+    rules: {
+        "vue-scoped-css/require-selector-used-inside": "error",
+        "vue-scoped-css/v-deep-pseudo-style": ["error", ":deep"],
+        "vue-scoped-css/v-global-pseudo-style": ["error", ":global"],
+        "vue-scoped-css/v-slotted-pseudo-style": ["error", ":slotted"],
+
+        "vue/block-tag-newline": "error",
+        "vue/html-button-has-type": "error",
+        "vue/html-comment-content-newline": "error",
+        "vue/html-comment-content-spacing": "error",
+        "vue/html-comment-indent": "error",
+        "vue/html-indent": "off",
+        "vue/next-tick-style": ["error", "promise"],
+        "vue/no-duplicate-attr-inheritance": "error",
+        "vue/no-irregular-whitespace": "error",
+        "vue/no-multiple-objects-in-class": "error",
+        "vue/no-potential-component-option-typo": [
+            "error",
+            {
+                presets: ["all"]
+            }
+        ],
+        "vue/no-static-inline-styles": [
+            "error",
+            {
+                allowBinding: true
+            }
+        ],
+        "vue/no-template-target-blank": "error",
+        "vue/no-useless-mustaches": "error",
+        "vue/padding-line-between-blocks": "error",
+        "vue/require-explicit-emits": "error"
+    }
+};
